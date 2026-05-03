dde76bbccfea79552619a9e5922de04c50de14b2
axrorback <axrorback@gmail.com>
2026-05-03 21:05:17 +0500

    Add new URL routes for profile, donation, and donation callback pages

--- asosiy/urls.py
+++ asosiy/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
-
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('robots.txt',robots_txt,name='robots'),
+    path('profile/<str:username>/',profile_view,name='profile'),
+    path('donate/',donate_page,name='donate'),
+    path('callback-donate/',donation_callback,name='callback_donate')
 ]
