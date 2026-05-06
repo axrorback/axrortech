7767976735842d537a09a16bad96455f5db1f317
axrorback <axrorback@gmail.com>
2026-05-06 23:13:07 +0500

    Register `UserMessage` and `TelegramUser` models in admin; add `ready` method to load handlers in "bot" app.

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bot.models import UserMessage , TelegramUser
 
-# Register your models here.
+@admin.register(UserMessage)
+class UserMessageAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'username', 'created_at')
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('telegram_id', 'username', 'created_at',)
